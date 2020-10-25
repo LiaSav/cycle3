1c74a16cedf49177483a0c85987318ad2528c512
LiaSav <liasav19@ukr.net>
2020-10-25 22:05:39 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// The function print a string with numbers from A to B separated by spaces.
+function cycle(a, b) {
+  let x = "";
+  for (let i = a; i <= b; i++) {
+    x += i + " ";
+  }
+  return x;
+}
+
